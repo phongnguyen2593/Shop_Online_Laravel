71506076fc9bf3984f1dcbd5dd525aec62b56633
phongnguyen2593 <phongnguyen2593@gmail.com>
2020-11-05 10:29:36 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group([
+    'namespace' => 'Backend',
+    'prefix' => 'admin',
+    'middleware' => 'auth', 
+    'as' => 'backend.'
+], function () {
+    Route::get('/dashboard', 'DashboardController@index')->name('index');
+});
