e9676949e01cc73b5223fb7ba35e89afbfa5948c
phongnguyen2593 <phongnguyen2593@gmail.com>
2020-11-12 03:25:55 +0700

    Homework unit 12

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\StoreProductRequest;
 use App\Models\Product;
 use App\Models\Category;
+use App\Models\Image;
 
 
 class ProductController extends Controller
@@ -47,15 +49,32 @@ class ProductController extends Controller
         $product->slug = \Illuminate\Support\Str::slug($request->get('name'));
         $product->quantity = $request->get('quantity');
         $product->category_id = $request->get('category_id');
-        $product->origin_price = $request->get('origin_price');
-        $product->sale_price = $request->get('sale_price');
-        $product->discount_percent = $request->get('discount_percent');
-        $product->content = $request->get('content');
+        $file = $request->file('thumbnail');
+        $path = Storage::disk('public')->putFileAs('thumbnail_product', $file, $file->getClientOriginalName());
+        $product->thumbnail = $path;
+        // $product->origin_price = $request->get('origin_price');
+        // $product->sale_price = $request->get('sale_price');
+        // $product->discount_percent = $request->get('discount_percent');
+        $product->description = $request->get('description');
         $product->status = $request->get('status');
         $product->user_id = Auth::user()->id;
         $product->save();
 
+        
+
+        if ($request->hasFile('images')) {
+            $images = $request->file('images');
+            foreach ($images as $image){
+                $path = Storage::disk('public')->putFileAs('product_images', $image, $image->getClientOriginalName());
+                $image = new Image();
+                $image->path = $path;
+                $image->product_id = $product->id;
+                $image->save();
+            }
+        }
+
         return redirect()->route('backend.product.index');
+        
     }
 
     /**
@@ -66,7 +85,12 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        $product = Product::find($id);
+        $images = $product->images()->where('product_id', $id)->get();
+        return view('backend.products.show', [
+            'product' => $product,
+            'images' =>$images
+            ]);
     }
 
     /**
@@ -118,6 +142,24 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            $product = Product::find($id);
+            $success = $product->delete();
+
+            if($success){
+                return response()->json([
+                    'error'=>false,
+                    'message'=>"Đã xóa",
+                ]);
+                location.reload();
+            }
+
+        }catch (\Exception $e){
+            $message = "Xóa không thành công";
+            return response()->json([
+                'error'=>true,
+                'message'=>$e->getMessage(),
+            ]);
+        }
     }
 }
